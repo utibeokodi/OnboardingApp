8a0354218383442378c7a07674fb243e692229d7
utibeokodi <utibeokodi@gmail.com>
2020-10-04 22:25:10 +0100

    New Update

--- OnboardingApp/settings.py
+++ OnboardingApp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i%arenw9rb$*s!(k=e#7rl4$!ho27p#k+gu+=k61n&sy+i5hxu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'onboardingapputibe.herokuapp.com']
 
