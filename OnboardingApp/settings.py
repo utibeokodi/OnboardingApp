e75399b6f3e7910cae79f99ca13c4565d213d59f
utibeokodi <utibeokodi@gmail.com>
2020-10-04 16:47:59 +0100

    New Update

--- OnboardingApp/settings.py
+++ OnboardingApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i%arenw9rb$*s!(k=e#7rl4$!ho27p#k+gu+=k61n&sy+i5hxu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
